ea5e5c2e7f4c2584351a66cfe14242fc3144c2d4
Wagner Torres <wstorres@gmail.com>
2021-02-17 11:07:29 -0300

    Criado o primeiro teste de API

---
+++ src/test/java/tasks/apitest/APITest.java
@@ -0,0 +1,53 @@
+package tasks.apitest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.apache.http.protocol.HTTP;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class APITest {
+
+    @BeforeAll
+    public static void steup(){
+        RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+    }
+
+    @Test
+    public void testDeveRetornarTarefas(){
+        RestAssured
+            .given()
+            .when()
+                .get("/todo/1")
+            .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void testDeveAdicionarTarefaComSucesso(){
+        RestAssured
+            .given()
+                .contentType(ContentType.JSON)
+                .body("{\"task\": \"Teste via API\", \"dueDate\": \"2021-02-18\"}")
+            .when()
+                .post("/todo/")
+            .then()
+                .statusCode(HttpStatus.SC_CREATED);
+    }
+
+    @Test
+    public void testNaoDeveAdicionarTarefaInvalida(){
+        RestAssured
+            .given()
+                .contentType(ContentType.JSON)
+                .body("{\"task\": \"Teste via API\", \"dueDate\": \"2010-12-30\"}")
+            .when()
+                .post("/todo/")
+            .then()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body("message", CoreMatchers.is("Due date must not be in past"));
+    }
+}
